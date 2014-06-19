7ce01034f81bfa571cf77f5e14f2bf081966fe95
Alex <alex@wp-themes-pro.com>
2014-06-19 12:19:09 +0200

    Secure Cocorico loading

--- cocorico-social.php
+++ cocorico-social.php
@@ -24,7 +24,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-require_once 'admin/Cocorico/Cocorico.php';
+if(is_admin())
+	require_once 'admin/Cocorico/Cocorico.php';
 
 // Load Styles
 function coco_social_load_style() {
